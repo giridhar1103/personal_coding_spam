0745f50a71167d110d731ea89ce6d1373019b9ca
giridhar1103 <giridharanand76@gmail.com>
2025-07-18 15:15:57 -0700

    Daily push - 2025-07-18

---
+++ InsertionSort.py
@@ -0,0 +1,13 @@
+def SSort(lst):
+    for i in range(1,len(lst)):
+        j = i-1
+        while j>=0 and lst[j+1]<lst[j]:
+            temp = j+1
+            lst[j+1] = lst[j]
+            lst[j] = temp
+
+            j-=1
+    return lst
+
+
+print(SSort([3,4,5,8,1]))
