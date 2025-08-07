507686b24da1c411f22288c51d56b5b396cf9ce8
giridhar1103 <giridharanand76@gmail.com>
2025-08-07 01:55:38 -0700

    Daily push - 2025-08-07

--- insTest.py
+++ insTest.py
@@ -3,5 +3,12 @@ lst = [1,3,2,7,4]
 def insTest(lst):
     for i in range(1,len(lst)):
         j = i-1
-        while j>=0 and lst[j]<lst[i]:
-            if
+        while j>=0 and lst[j]>lst[j+1]:
+            temp = lst[j]
+            lst[j] = lst[j+1]
+            lst[j+1] = temp
+
+            j -= 1
+    return lst
+
+print(insTest(lst))
